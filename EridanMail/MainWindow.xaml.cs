81378415075186e56e3543945dae699bda616751
lostsky25 <57411317+lostsky25@users.noreply.github.com>
2022-07-14 19:30:59 +0300

    Initial commit.
    * Welcome page almost complete. Animations are working.

---
+++ EridanMail/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EridanMail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PageWelcome pageWelcome;
+        public MainWindow()
+        {
+            InitializeComponent();
+            pageWelcome = new PageWelcome();
+            frMain.Navigate(pageWelcome);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Users\User\Desktop\chromewin\chrome.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            Process p = new Process();
+            startInfo.Arguments = "--app=https://google.com";
+            p.StartInfo = startInfo;
+            p.Start();
+            p.WaitForExit();
+
+            return;
+        }
+
+        private void Button_Click_Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void gridTitle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            xmlMainWindow.BorderThickness = new Thickness(0, 0, 0, 0);
+        }
+
+        private void xmlMainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            xmlMainWindow.BorderThickness = new Thickness(1, 1, 1, 1);
+        }
+
+        private void xmlMainWindow_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            xmlMainWindow.BorderThickness = new Thickness(1, 1, 1, 1);
+        }
+    }
+}
---
+++ EridanMail/PageWelcome.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EridanMail
+{
+    /// <summary>
+    /// Interaction logic for PageWelcome.xaml
+    /// </summary>
+    public partial class PageWelcome : Page
+    {
+        MainWindow Form = Application.Current.Windows[0] as MainWindow;
+        public PageWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Continue(object sender, RoutedEventArgs e)
+        {
+            //Form.frMain.Navigate(new PageWelcome());
+        }
+    }
+}
